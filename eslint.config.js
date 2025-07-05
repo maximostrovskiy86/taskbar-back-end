a9b6943acc6b195769f11bed40c7d10a5adf0622
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2025-07-05 10:51:45 +0200

    fix: fix .env and package json

--- eslint.config.js
+++ eslint.config.js
@@ -6,4 +6,23 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: ["plugin:react/recommended", "airbnb", "prettier"],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"]
+    }
+  }
 ]);
